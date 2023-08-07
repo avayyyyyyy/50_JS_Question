e1679b41881333e9b8dc7ab348a377bedb1597fa
avayyyyyyy <jabhi465@gmail.com>
2023-08-07 16:09:20 +0530

    Update

--- 13.js
+++ 13.js
@@ -1,1 +1,15 @@
-// <!-- calculates the factorial -->
+function factorial(num) {
+  let fact = 1;
+
+  if (num == 0 || num == 1) {
+    fact = 1;
+  } else {
+    for (let i = 1; i <= num; i++) {
+      fact = fact * i;
+    }
+  }
+
+  console.log(fact);
+}
+
+factorial(5);
